f401487b15e9eefcc2cbf34ae8ae494573324891
wcwei <wcwei@iflytek.com>
2022-07-20 15:31:58 +0800

    初始化项目结构

---
+++ gray-gateway/src/main/java/com/hanyi/gateway/GrayGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.hanyi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 网关启动类
+ * </p>
+ *
+ * @author wcwei@iflytek.com
+ * @since 2022-07-20 14:49
+ */
+@SpringBootApplication
+public class GrayGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrayGatewayApplication.class);
+    }
+
+}
---
+++ hanyi-privoder/src/main/java/com/hanyi/producer/ProducerApplication.java
@@ -0,0 +1,21 @@
+package com.hanyi.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 生产者启动类
+ * </p>
+ *
+ * @author wcwei@iflytek.com
+ * @since 2022-07-20 15:02
+ */
+@SpringBootApplication
+public class ProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class);
+    }
+
+}
